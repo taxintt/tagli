3bcb5c20763a275ff13a32f070bbb9aee3492c17
taxintt <nthouse14@gmail.com>
2023-03-12 16:23:24 +0900

    Use github.com/go-git/go-git/v5 to get git repository info

--- cmd/list.go
+++ cmd/list.go
@@ -1,8 +1,14 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
+	"io"
+	"log"
+	"os"
 
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/spf13/cobra"
 )
 
@@ -15,7 +21,55 @@ var encodeCmd = &cobra.Command{
 }
 
 func listTags() {
-	fmt.Println("This is the list of repository :)")
+	appPath := "."
+	r, err := git.PlainOpen(appPath)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	hashCommit, err := r.Head()
+	if err != nil {
+		log.Fatal("head", err)
+	}
+	fmt.Println("hash", hashCommit)
+
+	tags := make(map[plumbing.Hash]string)
+
+	iter, err := r.Tags()
+	if err != nil {
+		os.Exit(1)
+	}
+
+	for {
+		ref, err := iter.Next()
+		if errors.Is(err, io.EOF) {
+			break
+		}
+		if err != nil {
+			os.Exit(1)
+		}
+		tags[ref.Hash()] = ref.Name().Short()
+
+	}
+
+	cIter, err := r.Log(&git.LogOptions{From: hashCommit.Hash()})
+	for {
+		commit, err := cIter.Next()
+		if errors.Is(err, io.EOF) {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if str, ok := tags[commit.Hash]; ok {
+			fmt.Printf("%s-%s\n", str, hashCommit.Hash().String()[:8])
+			return
+		}
+	}
+
+	fmt.Println(hashCommit.Hash().String()[:8])
+	os.Exit(0)
 }
 
 func init() {
