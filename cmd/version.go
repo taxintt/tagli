50f2e9a633b7c75ad73296de1869b773f962f879
taxintt <nthouse14@gmail.com>
2023-08-01 20:35:25 +0900

    fix: modify not to use debug.ReadBuildInfo()

--- cmd/version.go
+++ cmd/version.go
@@ -3,12 +3,13 @@ package cmd
 import (
 	"fmt"
 	"runtime"
-	"runtime/debug"
 
 	"github.com/spf13/cobra"
 )
 
-var Version string = ""
+var (
+	Version string
+)
 
 func getVersion(version string) string {
 	return fmt.Sprintf(`tagli
@@ -21,11 +22,6 @@ Arch: %s`, version, runtime.GOOS, runtime.GOARCH)
 var versionCmd = &cobra.Command{
 	Use: "version",
 	Run: func(cmd *cobra.Command, args []string) {
-		info, ok := debug.ReadBuildInfo()
-		if !ok {
-			Version = "unknown"
-		}
-		Version = info.Main.Version
 		fmt.Println(getVersion(Version))
 	},
 	Short: "Show cli version info",
